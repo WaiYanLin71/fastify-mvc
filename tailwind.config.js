ed69ee173ee59a15b4a2496cc36ae3e128a49c6b
HsuMonLynn <scm.hml.hsumonlynn4@gmail.com>
2023-05-12 14:24:09 +0630

    add index design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {},
     container:{
       center:true
-    }
+    },
+    screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+		},
   },
   plugins: [],
 }
