bb963dc5a5927ac87aeca473a2b06a36643ab48c
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2023-05-23 16:50:10 +0630

    t

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDatabase = async (cb) => {
     const uri = process.env.APP_ENV === 'local' ? process.env.LOCAL_DATABASE : process.env.SERVER_DATABASE;
     try {
-        await mongoose.connect(uri)
+        await mongoose.connect('mongodb+srv://wai-yan-lin:bEIWbQ7EWqvQUnNB@cluster0.8tvlb2e.mongodb.net/?retryWrites=true&w=majority')
         cb(false)
     } catch (error) {
         cb(error)
