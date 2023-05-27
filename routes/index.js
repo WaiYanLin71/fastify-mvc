f884371d2f0ebcbd888b3280e578faccf8fe61ce
WaiYanLinn <waiyanlinn@outlook.com>
2023-05-27 13:14:45 +0630

    update prehandler

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,21 @@ import v1UserRoute from './v1/user.js'
 import v1AuthRoute from './v1/auth.js'
 import { decryptToken } from '../helper/jwt.js';
 
-export default  (fastify) => {
-
+export default (fastify) => {
     fastify.get('/', (req, reply) => {
         return reply.redirect('/v1/users')
     });
-
     fastify.addHook('preHandler', async (req, reply) => {
         let user = false;
-        if (req.cookies?.admin) {
-            try {
-                user = await decryptToken(req.cookies.admin)
-            } catch (error) {
-                await reply.status(440)
-                    .clearCookie('admin')
-                    .redirect('/v1/auth/login')
+        if ((/^\/v1\/(?!auth\b)[^/]+$/).test(req.url)) {
+            if (req.cookies?.admin) {
+                try {
+                    user = await decryptToken(req.cookies.admin)
+                } catch (error) {
+                    await reply.status(400)
+                        .clearCookie('admin')
+                        .redirect('/v1/auth/login')
+                }
             }
         }
         reply.locals = {
@@ -25,7 +25,7 @@ export default  (fastify) => {
                 query: req.query
             }
         }
-    }, { prefix: /^v1\/.*/ })
+    })
 
     fastify.register(v1UserRoute, { prefix: 'v1/users' })
     fastify.register(v1AuthRoute, { prefix: 'v1/auth' })
