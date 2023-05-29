467d476afa83a107403ea361fcd335c23a904d65
WaiYanLinn <waiyanlinn.lion@outlook.com>
2023-05-29 10:02:07 +0630

    udpate tailwind css

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,8 +1,9 @@
 const mix = require('laravel-mix');
 const path = require('path');
-const tailwindcss = require('tailwindcss');
+require('mix-tailwindcss');
 const rootDir = path.resolve(__dirname);
 mix.setPublicPath(rootDir)
-mix.postCss('public/assets/css/tailwind.css', 'public/assets/css/index.css', [
-    tailwindcss('tailwind.config.js'),
-]).js('public/assets/app.js', 'public/assets/js/app.js').version()
+mix.postCss('public/assets/css/tailwind.css', 'public/assets/css/index.css')
+.tailwind('./tailwind.config.js'),
+
+mix.js('public/assets/app.js', 'public/assets/js/app.js').version()
